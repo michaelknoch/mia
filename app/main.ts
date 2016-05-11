c5ad1e538256c5c079e73601e08c0935248fe1ca
Michael Knoch <info@michaelknoch.de>
2016-05-11 02:34:25 +0200

    update maints

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,34 @@
 ///<reference path="../typings/browser.d.ts" />
 
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
-bootstrap(AppComponent);
+import {AppComponent} from './app.component';
+import {provide} from '@angular/core';
+import {bootstrap}    from '@angular/platform-browser-dynamic';
+import {ROUTER_PROVIDERS} from '@angular/router';
+
+import {HTTP_PROVIDERS} from '@angular/http';
+import {CostumerService} from './service/costumer/costumer.service';
+import {UserService} from "./service/user/user.service";
+
+import {CompanyService} from "./service/company/company.service";
+import {DataService} from "./service/data.service";
+
+import {
+    PlatformLocation,
+    Location,
+    LocationStrategy,
+    HashLocationStrategy,
+    PathLocationStrategy,
+    APP_BASE_HREF }
+    from '@angular/common';
+
+
+bootstrap(AppComponent, [
+    ROUTER_PROVIDERS,
+    HTTP_PROVIDERS,
+    CostumerService,
+    UserService,
+    CompanyService,
+    DataService,
+    provide(LocationStrategy, {useClass: HashLocationStrategy}),
+    provide(APP_BASE_HREF, { useValue: '/' })
+]);
