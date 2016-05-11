3247bfba31d09ad68c1296406c3e6c389fc381b5
Michael Knoch <info@michaelknoch.de>
2016-05-11 02:33:33 +0200

    refactor app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig} from '@angular/router-deprecated';
+import {Login} from './comp/login/login.comp';
+import {Root} from './comp/root/root.comp';
+import {CompanyList} from "./comp/companyList/companyList.comp";
+import {NewCompany} from "./comp/newCompany/newCompany.comp";
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    moduleId: module.id,
+    selector: 'dash',
+    templateUrl: 'app.html',
+    directives: [ROUTER_DIRECTIVES],
+    providers: [ROUTER_PROVIDERS]
 })
 
-export class AppComponent { }
+@RouteConfig([
+    {
+        path: '/',
+        component: Login
+    },
+    {
+        path: '/companyList',
+        component: CompanyList},
+    {
+        path: '/new-company',
+        component: NewCompany},
+    {
+        path: '/root/...',
+        component: Root
+    }
+])
+
+
+
+
+export class AppComponent {
+
+}
