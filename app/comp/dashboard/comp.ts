8be255330a39a2d3abb0133128628b4ea0bc00a3
Michael Knoch <info@michaelknoch.de>
2016-05-13 13:41:44 +0200

    improve dashboard and show dummy chart

--- app/comp/dashboard/comp.ts
+++ app/comp/dashboard/comp.ts
@@ -1,19 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {ZingChart} from "../zingChart/zingChart.comp";
+
 
 @Component({
-    template: `
-
-     `
-
+    moduleId: module.id,
+    directives: [ZingChart],
+    templateUrl: 'dashboard.html',
 })
 
-export class Dashboard implements OnInit {
+export class Dashboard {
+
+    charts;
 
     constructor() {
+
+        this.charts = [{
+            "id": 'chart-1',
+            "data": {
+                "type": 'line',
+                "series": [{
+                    "values": [2, 3, 4, 5, 3, 3, 2]
+                }],
+            },
+            "height": 400,
+            "width": 600
+        }]
+
     }
 
-    ngOnInit() {
-    }
 
 }
 
