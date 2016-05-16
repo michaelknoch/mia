6f59618cb575db4a7c85e39f210ca20b11faf78a
Michael Knoch <info@michaelknoch.de>
2016-05-16 17:27:58 +0200

    improve metrics

--- app/comp/metrics/metrics.comp.ts
+++ app/comp/metrics/metrics.comp.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core'
 import {ROUTER_DIRECTIVES} from '@angular/router-deprecated';
 import {ApplicationService} from "../../service/application/application.service";
+import {MetricService} from "../../service/metric/metric.service";
+import {CHART_DIRECTIVES} from '../chartJS/charts';
 
 @Component({
     moduleId: module.id,
     selector: 'metrics',
-    directives: [ROUTER_DIRECTIVES],
+    directives: [ROUTER_DIRECTIVES, CHART_DIRECTIVES],
     templateUrl: 'metrics.html',
     styleUrls: ['metrics.css'],
 })
@@ -13,18 +15,66 @@ import {ApplicationService} from "../../service/application/application.service"
 export class Metrics {
 
     applications;
+    chartData;
 
-    constructor(private _ApplicationService: ApplicationService) {
+
+    constructor(private _ApplicationService: ApplicationService, private _MetricService: MetricService) {
 
         _ApplicationService.getApplications().subscribe(data => {
             this.applications = data;
             console.info(this.applications);
+
+            this.selectApplication(this.applications[0]._id)
+        });
+
+    }
+
+    selectApplication(id) {
+        console.log(id);
+
+        this._MetricService.getLoadAvg(id).subscribe(data => {
+            console.info(data);
+            this.chartData = this.processData(data, 'LoadAvg');
+
         })
 
     }
 
-    selectApplication(value) {
-        console.log(value);
+    processData(data: any, name: String) {
+
+        var labels = [];
+        var values = [];
+
+        for (var item of data[0].series[0].values) {
+            labels.push(item[0]);
+            values.push(item[1]);
+        }
+
+        var result = {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: name,
+                    data: values,
+                    borderColor: "rgba(75,192,192,1)",
+                }]
+            },
+            options: {
+                responsive: false,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: false
+                        }
+                    }]
+                }
+            }
+        };
+
+        console.info(result);
+        return result;
     }
+
 
 }
