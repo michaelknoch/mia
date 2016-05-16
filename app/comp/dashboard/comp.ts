85c4f474d775aade493dddb98c6c875d2c28d001
Michael Knoch <info@michaelknoch.de>
2016-05-16 16:23:11 +0200

    improve root and dashboard

--- app/comp/dashboard/comp.ts
+++ app/comp/dashboard/comp.ts
@@ -1,31 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import {ZingChart} from "../zingChart/zingChart.comp";
+import { Component, OnInit} from '@angular/core';
+import {CHART_DIRECTIVES} from '../chartJS/charts';
+import {CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass} from '@angular/common';
 
 
 @Component({
     moduleId: module.id,
-    directives: [ZingChart],
+    directives: [CHART_DIRECTIVES, NgClass, CORE_DIRECTIVES, FORM_DIRECTIVES],
     templateUrl: 'dashboard.html',
+    styleUrls: ['dashboard.css']
 })
 
 export class Dashboard {
 
-    charts;
-
     constructor() {
+    }
 
-        this.charts = [{
-            "id": 'chart-1',
-            "data": {
-                "type": 'line',
-                "series": [{
-                    "values": [2, 3, 4, 5, 3, 3, 2]
-                }],
-            },
-            "height": 400,
-            "width": 600
-        }]
-
+    public data = {
+        type: 'line',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderColor: "rgba(75,192,192,1)",
+            }]
+        },
+        options: {
+            responsive: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
     }
 
 
