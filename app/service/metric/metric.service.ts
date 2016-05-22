685b550e0a5446cff3e9516f5d45143fff439890
Michael Knoch <info@michaelknoch.de>
2016-05-22 16:44:01 +0200

    modify query parameters

--- app/service/metric/metric.service.ts
+++ app/service/metric/metric.service.ts
@@ -11,12 +11,12 @@ export class MetricService {
     }
 
     getLoadAvg(id: String) {
-        return this.http.get(Config.BASEPATH + '/metrics/applications/' + id + '/loadavg?period=3h')
+        return this.http.get(Config.BASEPATH + '/metrics/applications/' + id + '/loadavg?aggregate_fn=mean&group_by_value=10&group_by_unit=m&period=3h')
             .map(res => res.json())
     }
 
     getMemory(id: String) {
-        return this.http.get(Config.BASEPATH + '/metrics/applications/' + id + '/memory')
+        return this.http.get(Config.BASEPATH + '/metrics/applications/' + id + '/memory?period=5m')
             .map(res => res.json())
     }
 
