4082b8ecfa35ff85764c582f8c5fd6062460c1bf
Michael Knoch <info@michaelknoch.de>
2016-06-16 12:04:49 +0200

    encapsulate login component from loginview

--- app/comp/login/login.comp.ts
+++ app/comp/login/login.comp.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router-deprecated';
 import {UserService} from "../../service/user/user.service";
-import {DataService} from "../../service/data.service";
 import {LocalStorage, SessionStorage} from "angular2-localstorage/WebStorage";
+import {LoginRegister} from "./loginRegister/loginRegister.comp";
 
 @Component({
     moduleId: module.id,
     templateUrl: 'login.html',
     styleUrls: ['login.css'],
+    providers: [UserService],
+    directives: [LoginRegister]
 })
 
 export class Login implements OnInit {
 
-    loginState: Boolean = true;
-
-    password: String;
-    mail: String;
-    name: String;
-    surname: String;
+    constructor(private _userService: UserService, private _router: Router) {
+    }
 
     ngOnInit() {
 
@@ -31,39 +29,10 @@ export class Login implements OnInit {
         } else if (!system && user) {
             this._router.navigate(['System-list']);
         }
-
     }
 
-    constructor(private _router: Router, private _userService: UserService) {
-    }
-
-    login() {
-        this._userService.login(this.mail, this.password).subscribe(
-            data => {
-                this._userService.setUser({name: data.name, id: data._id});
-                this._router.navigate(['System-list']);
-            },
-            err => this.err(err));
-
-    }
-
-    logout() {
-        this._userService.logout().subscribe(data => {
-            console.info('logout');
-        });
-    }
-
-    register() {
-        this._userService.register(this.mail, this.password, this.name, this.surname).subscribe(
-            data => {
-                this._router.navigate(['System-list']);
-                this._userService.setUser({name: data.name, id: data._id});
-            },
-            err => this.err(err));
-    }
-
-    err(msg) {
-        console.error(msg);
+    private loginHandler(data: any) {
+        this._router.navigate(['System-list']);
     }
 
 }
---
+++ app/comp/login/loginRegister/loginRegister.comp.ts
@@ -0,0 +1,54 @@
+import {Component, Output, EventEmitter} from '@angular/core'
+import {UserService} from "../../../service/user/user.service";
+
+@Component({
+    moduleId: module.id,
+    selector: 'login-register',
+    templateUrl: 'loginRegister.html',
+    styleUrls: ['loginRegister.css'],
+})
+
+export class LoginRegister {
+
+    @Output() public loginEvent: EventEmitter<any> = new EventEmitter();
+
+    private loginState: Boolean = true;
+    private password: String;
+    private mail: String;
+    private name: String;
+    private surname: String;
+
+
+    constructor(private _userService: UserService) {
+    }
+
+    private login() {
+        this._userService.login(this.mail, this.password).subscribe(
+            data => {
+                this.emit(data);
+            },
+            err => err(err));
+    }
+
+    private register() {
+        this._userService.register(this.mail, this.password, this.name, this.surname).subscribe(
+            data => {
+                this.emit(data);
+            },
+            err => err(err));
+    }
+
+    static err(msg) {
+        console.error(msg);
+    }
+
+    private emit(data) {
+        this._userService.setUser({name: data.name, id: data._id});
+        this.loginEvent.emit({
+            name: data.name,
+            id: data._id,
+            status: 200
+        });
+    }
+
+}
