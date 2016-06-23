4490265d5631c8178b606f42556d2f69d39b621f
Michael Knoch <info@michaelknoch.de>
2016-06-23 16:37:39 +0200

    add images hack, should be removed soon

--- app/comp/root/root.comp.ts
+++ app/comp/root/root.comp.ts
@@ -38,10 +38,18 @@ export class Root implements OnInit {
     private username: string;
     private systemname: string;
 
+    private images = {
+        'Timo': 'https://scontent.xx.fbcdn.net/v/t1.0-1/p160x160/1474558_1207958742552423_6007711756409685465_n.jpg?oh=0eee20ff133ccba7335bcf6a9040fb74&oe=580A1AD8',
+        'Michael': 'https://scontent.xx.fbcdn.net/v/t1.0-1/p160x160/943793_1280165198665216_4684989572148252159_n.jpg?oh=5dc9689a604fba7044369f29f918afe4&oe=57C1E3B8'
+    };
+
     ngOnInit() {
         this.username = this._userService.getLocalMe().user.name;
         this.systemname = this._userService.getLocalMe().system.name;
+    }
 
+    getUserThumb() {
+        return this.images[this.username]
     }
 
     constructor(private _userService: UserService, private _router: Router) {
