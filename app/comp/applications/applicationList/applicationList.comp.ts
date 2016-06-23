cc6fc50db82ab435bae1d0e0c4ba4c4e6de144de
Michael Knoch <info@michaelknoch.de>
2016-06-23 15:58:23 +0200

    use applicationmodal

--- app/comp/applications/applicationList/applicationList.comp.ts
+++ app/comp/applications/applicationList/applicationList.comp.ts
@@ -1,9 +1,11 @@
-import {Component} from '@angular/core'
-import {ApplicationService} from "../../../service/application/application.service";
+import {Component, ViewChild} from '@angular/core'
+import {ApplicationService} from '../../../service/application/application.service';
+import {NewApplicationModal} from '../../../../dist/comp/applications/newApplicationModal/newApplicationModal.comp';
 
 @Component({
     moduleId: module.id,
     selector: 'application-list',
+    directives: [NewApplicationModal],
     templateUrl: 'applicationList.html',
     styleUrls: ['applicationList.css']
 })
@@ -11,17 +13,25 @@ import {ApplicationService} from "../../../service/application/application.servi
 export class ApplicationList {
 
     private applications = [];
+    @ViewChild(NewApplicationModal) _newApplicationModal;
 
     constructor(private _ApplicationService: ApplicationService) {
+        this.getData()
+    }
 
-        _ApplicationService.getApplications().subscribe(
+    getData() {
+        this._ApplicationService.getApplications().subscribe(
             data => {
                 this.applications = data;
                 console.info(data);
             },
             err => console.error(err)
         );
+    }
 
+    update(data: any) {
+        console.info(data);
+        this.getData()
     }
 
 }
