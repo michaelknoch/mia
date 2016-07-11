0ea7cfe914a54c3b170a5ed24933ed50e7ed84f6
Michael Knoch <info@michaelknoch.de>
2016-07-11 19:16:02 +0200

    implement feed

--- app/comp/feed/feed.comp.ts
+++ app/comp/feed/feed.comp.ts
@@ -1,16 +1,40 @@
 import {Component} from '@angular/core'
 import {ROUTER_DIRECTIVES} from '@angular/router-deprecated';
+import {MetricService} from "../../service/metric/metric.service";
+import {ApplicationMetaPicker} from "../applicationMetaPicker/applicationMetaPicker.comp";
+
+declare var moment;
 
 @Component({
     moduleId: module.id,
     selector: 'feed',
-    directives: [ROUTER_DIRECTIVES],
+    directives: [ROUTER_DIRECTIVES, ApplicationMetaPicker],
     templateUrl: 'feed.html',
     styleUrls: ['feed.css'],
 })
 
 export class Feed {
 
-    constructor() {}
+    private startStops = [];
+
+    constructor(private _metricService: MetricService) {
+
+    }
+
+    private getData(appId: string, query: string) {
+        this._metricService.getMetrics(appId, query).subscribe((data: any) => {
+            this.startStops = data.startStop;
+            console.info(this.startStops);
+        });
+    }
+
+    public metaUpdate(e: any) {
+        this.getData(e.appId, e.query);
+        console.log(e);
+    }
+
+    private getMoment(isoDate: string) {
+        return moment(new Date(isoDate)).format('MMMM Do YYYY, h:mm:ss a')
+    }
 
 }
