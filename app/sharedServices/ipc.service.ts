3c0683dcd22a0ad879e01d19f186f0b11ec27125
Michael Knoch <info@michaelknoch.de>
2016-08-05 16:14:04 +0200

    implement notification and notify on update events

--- app/sharedServices/ipc.service.ts
+++ app/sharedServices/ipc.service.ts
@@ -18,27 +18,31 @@ export class IpcService {
             electron.ipcRenderer.on('update-available', () => {
                 this.mainProcessEvents.emit('update-available');
                 console.info('update-available');
+                this.notification('update available', 'Update')
             });
 
             electron.ipcRenderer.on('update-downloaded', () => {
                 this.mainProcessEvents.emit('update-downloaded');
                 console.info('update-downloaded');
+                this.notification('update downloaded', 'Update')
             });
 
             electron.ipcRenderer.on('version-receive', (event, version) => {
                 this.versionEmitter.emit({version: version});
                 console.info('version-receive', version);
-
-                let myNotification = new Notification('Appversion', {
-                    body: 'Version: ' + version,
-                    silent: true
-                });
-                new Audio('dist/assets/sounds/message.mp3').play();
-
+                this.notification('Version: ' + version, 'Appversion')
             });
 
             electron.ipcRenderer.send('version-request');
 
         }
+    }
+
+    private notification(body: string, title: string) {
+        let myNotification = new Notification(title, {
+            body: body,
+            silent: true
+        });
+        new Audio('dist/assets/sounds/message.mp3').play();
     }
 }
